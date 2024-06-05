b4094286ef332039c1351ad93ef2848e350cb469
Matilde93 <matildeaarenstrup@gmail.com>
2024-06-05 20:23:25 +0200

    all files added

---
+++ src/app/pages/activitypage/activitypage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activitypage',
+  standalone: true,
+  imports: [],
+  templateUrl: './activitypage.component.html',
+  styleUrl: './activitypage.component.css'
+})
+export class ActivitypageComponent {
+
+}
---
+++ src/app/pages/eventpage/eventpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventpage.component.html',
+  styleUrl: './eventpage.component.css'
+})
+export class EventpageComponent {
+
+}
