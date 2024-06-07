912c11d80ca04f49dd85d1102c655034e4728814
Matilde93 <matildeaarenstrup@gmail.com>
2024-06-07 20:06:46 +0200

    fixed routing scroll position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AuthService } from './services/auth/auth.service'; 
+
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,9 @@ export class AppComponent implements OnInit {
 
   authService = inject(AuthService)
 
+  constructor(private router: Router) {}
+
+  
   ngOnInit(): void {
       this.authService.user$.subscribe((user: { email: string; displayName: string; }) => {
         if (user){
@@ -28,6 +32,12 @@ export class AppComponent implements OnInit {
           this.authService.currentUserSig.set(null);
         }
         console.log("User logged in: " + this.authService.currentUserSig()?.name)
+      });
+
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          window.scrollTo(0, 0);
+        }
       });
   }
 }
